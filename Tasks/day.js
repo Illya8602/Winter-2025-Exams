fad3cb83a58cdb5c046e276922d9c5c311b2e43a
Illya8602 <illyacemchenko@gmail.com>
2025-01-15 18:22:12 +0200

    using findIndex instead of a loop

--- Tasks/day.js
+++ Tasks/day.js
@@ -4,13 +4,9 @@
 const DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 const getDayNumber = (dayString) => {
-  let i;
-  for (i = 0; i < DAYS.length; i++) {
-    if (dayString.startsWith(DAYS[i].toLowerCase())) {
-      return i + 1;
-    }
-  }
-  return -1;
+  const dayIndex = DAYS.findIndex(day =>
+    dayString.toLowerCase().startsWith(day.toLocaleLowerCase()))
+return dayIndex !== -1 ? dayIndex +1 : -1;
 };
 
 module.exports = getDayNumber;
