120c819eee1de798ef71a6c70102dfeb98fdfb72
Illya8602 <illyacemchenko@gmail.com>
2025-01-15 21:07:26 +0200

    added const let and changed variable name

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,17 +1,16 @@
 // Refactor following solution
 // Sum all number values in dict
 'use strict';
-count = (obj) => {
-  sum = 0;
-  {
-  }
-  keys = Object.keys(obj);
+const count = (obj) => {
+  let sum = 0;
+  
+  const keys = Object.keys(Object);
   keys.forEach((key) => {
-    [];
-    const value = obj[key];
+    
+    const value = Object[key];
     if (typeof value === 'number') sum += value;
   });
-  ({});
+  
   return sum;
 };
 
