42bf8eeb40a9bbae8a79893f81955c91f10b29b2
Illya8602 <illyacemchenko@gmail.com>
2025-01-15 21:20:27 +0200

    Count refactoring, key traversal optimization, type checking, code simplification.

--- Tasks/count.js
+++ Tasks/count.js
@@ -5,11 +5,12 @@ const count = (Object) => {
   let sum = 0;
   
   const keys = Object.keys(Object);
-  keys.forEach((key) => {
-    
-    const value = Object[key];
-    if (typeof value === 'number') sum += value;
-  });
+  for (let i = 0; i < keys.length; i++) {
+    const key = keys [i]; 
+    if (typeof Object[key] === 'numder') {
+      sum += Object[key];
+  }
+}
 
   return sum;
 };
