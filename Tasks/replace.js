9abf0356134a58a0081520733545ee4a4f508232
Illya8602 <illyacemchenko@gmail.com>
2025-01-15 21:53:08 +0200

    changed do while to while

--- Tasks/replace.js
+++ Tasks/replace.js
@@ -6,17 +6,14 @@ const replace = (str, substr, newstr) => {
     return str;
   let temp = str;
   let res = "";
-  do { 
-    let index = temp.indexOf(substr);
-    if (index === -1) {
-      return res + temp;
-    } else {
-      const start = temp.substring(0, index);
-      temp = temp.substring(index + substr.lenght, temp.lenght);
-      res += start + newstr;
-    }
-  } while (true);
-  
+  let index = temp.indexOf(substr);
+  while(index !== -1) {
+    const start = temp.substring(0, index);
+    temp = temp.substring(index + substr.length, temp.length);
+    res += start + newstr;
+    index = temp.indexOf(substr);
+  }
+  return res + temp; 
 };
 
 
